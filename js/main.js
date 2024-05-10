842c1eaaeb9979ad77d1061611a0ce0260288219
islom2202 <islomihsan@gmail.com>
2024-05-10 23:53:22 +0500

    added a font changing function in js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 import Theme from "./classes/theme.js"
 
 // selectors
-const bodyElement = document.querySelector("body")
+const bodyElement = document.body
 const switcher = document.querySelector(".js-switcher")
 const prefersDark = window.matchMedia("(prefers-color-scheme: dark)")
 const dropdown = document.querySelector(".dropdown")
+const allTags = document.getElementsByTagName("*")
 
 // switch theme & set theme based on users device preference
 switcher.onclick = (e) => new Theme(bodyElement, e.target).switchTheme()
@@ -20,6 +21,22 @@ function setWidth(){
     dropdown.style.width = dropdown.value.length + 1 + "rem"
   }
 }
+
+dropdown.onchange = () => changeFont()
+function changeFont(){
+  for(let i = 0; i <= allTags.length; i++){
+    if (dropdown.value == "sans-serif") {
+      allTags[i].style.setProperty("font-family", '"Inter", sans-serif')
+    }
+    if (dropdown.value == "serif") {
+      allTags[i].style.setProperty("font-family", '"Lora", serif')
+    }
+    if (dropdown.value == "mono") {
+      allTags[i].style.setProperty("font-family", '"Inconsolata", monospace')
+    }
+  }
+}
+
 
 
 
